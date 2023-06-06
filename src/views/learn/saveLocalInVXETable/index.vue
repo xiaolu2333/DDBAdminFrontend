dfe7f77c093d0dd4acce919ebec4c8502ac629c6
dangfulin <2694977226@qq.cmo>
2023-06-06 17:55:19 +0800

    fix:修复弹窗组件关闭时watch属性中进行的第二次请求。

--- src/views/learn/saveLocalInVXETable/index.vue
+++ src/views/learn/saveLocalInVXETable/index.vue
@@ -179,3 +179,222 @@ onMounted(() => {
 <style scoped>
 
 </style>
+
+
+<!--<template>-->
+<!--  <el-dialog-->
+<!--      title="【数据库管理员】用户授权"-->
+<!--      width='800px'-->
+<!--      :close-on-click-modal="false"-->
+<!--      v-model="dialogIsVisible"-->
+<!--      style="height: 680px; width: 700px;"-->
+<!--      :destroy-on-close="true"-->
+<!--      @close="closeDialog"-->
+<!--      @opened="openDialogHandler"-->
+<!--  >-->
+<!--    <div style="height: 510px;">-->
+<!--      <el-scrollbar max-height='500px'>-->
+<!--        <el-tree-->
+<!--            ref='userTreeRef'-->
+<!--            node-key='id'-->
+<!--            :data='userOptions'-->
+<!--            :check='checkedChange'-->
+<!--            show-checkbox-->
+<!--            :props="{ children: 'nodes', label: 'text', disabled: ''}"-->
+<!--            :expand-on-click-node='false'-->
+<!--            default-expand-all-->
+<!--        />-->
+<!--      </el-scrollbar>-->
+<!--    </div>-->
+<!--    <template #footer>-->
+<!--      <el-row>-->
+<!--        <el-col :span="12" style="text-align: left!important;">-->
+<!--          <el-button @click="closeDialog1" :icon="WarningFilled" :size="20"/>-->
+<!--          <el-button @click="closeDialog1" :icon="QuestionFilled" :size="20"/>-->
+<!--        </el-col>-->
+<!--        <el-col :span="12">-->
+<!--          <el-button @click="closeDialog" :icon="CircleClose">关闭</el-button>-->
+<!--          <el-button type='primary' @click='submit' :icon="Checked">确定</el-button>-->
+<!--        </el-col>-->
+<!--      </el-row>-->
+<!--    </template>-->
+<!--  </el-dialog>-->
+<!--</template>-->
+
+<!--<script lang="ts" setup>-->
+<!--import {onMounted, reactive, ref, toRefs, watch, computed} from "vue";-->
+<!--import {ElForm, ElMessage, ElTree} from 'element-plus'-->
+<!--import {Checked, QuestionFilled, WarningFilled, CircleClose} from '@element-plus/icons-vue'-->
+
+<!--import {getDbAdminUsersTree, saveDbAdminUsers} from '@/api/dbManage/registerCenter'-->
+<!--import {user} from "@/api/framework/resource/role/types";-->
+<!--import {isCenterAdmin, isRoleA, isRoleB, isRoleC, isSubAdmin} from "@/utils/auth";-->
+
+
+<!--// const props = defineProps<{-->
+<!--//   dialogData: {-->
+<!--//     dbid: undefined,-->
+<!--//     dialogVisible: boolean,-->
+<!--//   }-->
+<!--// }>()-->
+
+<!--const props = defineProps({-->
+<!--  dbid: {-->
+<!--    type: String,-->
+<!--    required: true-->
+<!--  },-->
+<!--  dialogVisible: {-->
+<!--    type: Boolean,-->
+<!--    default: false-->
+<!--  },-->
+<!--})-->
+
+<!--const dialogIsVisible = computed(() => {-->
+<!--  console.log('props.dialogVisible:', props.dialogVisible)-->
+<!--  return props.dialogVisible-->
+<!--})-->
+
+<!--const emit = defineEmits(['clean-dialog-data'])-->
+
+<!--const dataFormRef = ref(ElForm)-->
+<!--// 授权用户树-->
+<!--const userTreeRef = ref(ElTree)-->
+
+<!--const state = reactive({-->
+<!--  // 提示-->
+<!--  placeholder: "数据库管理员",-->
+<!--  // 可被分配权限的用户的列表-->
+<!--  userOptions: [] as any[],-->
+<!--  dialogVisible: false,-->
+<!--})-->
+
+<!--const {-->
+<!--  placeholder,-->
+<!--  userOptions,-->
+<!--  dialogVisible,-->
+<!--} = toRefs(state);-->
+
+<!--// watch(props, () => {-->
+<!--//   // console.log('子组件接受到props.dialogData：', props.dialogData)-->
+<!--//   state.dialogVisible = props.dialogVisible-->
+<!--//   getDbAdminUsersTree(props.dbid).then((response) => {-->
+<!--//     // console.log('response:', response)-->
+<!--//     userOptions.value = response.data-->
+<!--//-->
+<!--//     setNodesChecked()-->
+<!--//   })-->
+<!--// })-->
+
+
+<!--/********************************* tree事件 *********************************/-->
+<!--// 选中事件-->
+<!--function checkedChange(data: any, checked: any, indeterminate: any) {-->
+<!--  console.log('checkedChange:', data, checked, indeterminate)-->
+<!--}-->
+
+
+<!--/********************************* dialog事件 *********************************/-->
+<!--// 提交-->
+<!--const submit = () => {-->
+<!--  const userCodeList = getCheckedUsersCode()-->
+
+<!--  console.log('userCodeList:', userCodeList)-->
+<!--  saveDbAdminUsers(props.dbid, userCodeList).then((data) => {-->
+<!--    ElMessage.success('数据库管理员设置成功')-->
+<!--  }).finally(() => {-->
+<!--    closeDialog()-->
+<!--  })-->
+<!--}-->
+
+<!--function openDialogHandler() {-->
+<!--  init()-->
+<!--}-->
+
+<!--const closeDialog = () => {-->
+<!--  state.dialogVisible = false-->
+<!--  state.placeholder = ""-->
+<!--  state.userOptions = []-->
+<!--  console.log('子组件抛出：', state.dialogVisible)-->
+<!--  // emit('clean-dialog-data', state.dialogVisible, state.userOptions)-->
+<!--  emit("clean-dialog-data", "AToBDbTableAuth", false);-->
+<!--}-->
+
+
+<!--/**************************** utils ******************************/-->
+<!--// 根据 selectable 字段设置节点是否默认被选中-->
+<!--function setNodesChecked() {-->
+<!--  const list: any = []-->
+<!--  let roleUserList = check(state.userOptions, list)-->
+<!--  let listCode: any = []-->
+<!--  for (let i = 0; i < roleUserList.length; i++) {-->
+<!--    if (roleUserList[i].selectable == true) {-->
+<!--      listCode[i] = roleUserList[i].id-->
+<!--    }-->
+<!--  }-->
+<!--  // console.log('listCode:', listCode)-->
+<!--  userTreeRef.value!.setCheckedKeys(listCode, false)-->
+<!--}-->
+
+<!--// 递归树形数据-->
+<!--function check(data, list) {-->
+<!--  data.forEach((item) => {-->
+<!--    if (item.nodes && item.nodes.length > 0) {-->
+<!--      check(item.nodes, list)-->
+<!--    } else {-->
+<!--      list.push(item)-->
+<!--    }-->
+<!--  })-->
+<!--  return list-->
+<!--}-->
+
+<!--// 获取被选中的用户的code-->
+<!--function getCheckedUsersCode(): string {-->
+<!--  // 获取被选中的节点-->
+<!--  const userNode = userTreeRef.value!.getCheckedNodes(false, false)-->
+<!--  // console.log('userNode:', userNode)-->
+
+<!--  let userCodeNode: any = []-->
+<!--  let temp = 0-->
+<!--  for (let i = 0; i < userNode.length; i++) {-->
+<!--    // 从用户节点中获取用户code-->
+<!--    if (userNode[i].type == 'user') {-->
+<!--      userCodeNode[temp] = userNode[i].userCode-->
+<!--      temp++-->
+<!--    }-->
+<!--  }-->
+<!--  return userCodeNode.join(',')-->
+<!--}-->
+
+
+<!--/********************************* init *********************************/-->
+<!--// 表格当前选中行数据-->
+<!--const curRow: any = ref();-->
+<!--// 当前用户是否为中心管理员-->
+<!--const center_admin = isCenterAdmin();-->
+<!--// 当前用户是否为子管理员-->
+<!--const sub_admin = isSubAdmin();-->
+<!--// 当前用户是否为数据库系统管理员（A）-->
+<!--const db_sys_admin = isRoleA()-->
+<!--// 当前用户是否为数据库管理员（B）-->
+<!--const db_admin = isRoleB()-->
+<!--// 当前用户是否为数据库操作员（C）-->
+<!--const db_operator = isRoleC()-->
+
+<!--function init() {-->
+<!--  state.dialogVisible = dialogIsVisible.value-->
+<!--  getDbAdminUsersTree(props.dbid).then((response) => {-->
+<!--    // console.log('response:', response)-->
+<!--    userOptions.value = response.data-->
+
+<!--    setNodesChecked()-->
+<!--  })-->
+<!--  state.placeholder = db_sys_admin ? "数据库系统管理员" : "数据库操作员"-->
+<!--}-->
+
+
+<!--onMounted(() => {-->
+<!--});-->
+<!--</script>-->
+
+<!--<style scoped>-->
+<!--</style>-->
