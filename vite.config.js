fbc6def85393731905bf4b9d3f67bb904fce65a5
xiaolu2333 <2694977226@qq.com>
2023-06-29 23:51:00 +0800

    添加打包体积分析工具

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import {fileURLToPath, URL} from 'node:url';
 
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import bundleAnalyzer from "rollup-plugin-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+      bundleAnalyzer(),
+      vue()
+    ],
     resolve: {
         alias: {
             // 重命名路径别名
