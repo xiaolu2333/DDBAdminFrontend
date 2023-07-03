d59d3dbffddd8c2b64ed6a3ab0d2e83dda652976
dangfulin <2694977226@qq.cmo>
2023-07-03 13:50:01 +0800

    响应式布局：网格布局

--- src/views/learn/reactiveLayout/index.vue
+++ src/views/learn/reactiveLayout/index.vue
@@ -1,15 +1,47 @@
 <template>
-<div class="test">
-  11111
-</div>
+  <div>
+    <div class="container-1">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 </script>
 
-<style scoped>
-/* 响应式布局 */
-.test {
-  flex: 1;
+<style lang="scss" scoped>
+.container-1 {
+  background-color: #daf7d0;
+  // 使用 grid 布局
+  display: grid;
+  // 定义网格布局的列，grid-template-columns<rows>: length | percent | auto | fr
+  grid-template-columns: 1fr 1fr 1fr;
+  // 定义网格布局的行，grid-template-rows<rows>: length | percent | auto | fr
+  grid-template-rows: 1fr 1fr 1fr;
+  // 定义网格布局的列间距为 10px
+  grid-column-gap: 10px;
+  // 定义网格布局的行间距为 10px
+  grid-row-gap: 10px;
+}
+
+.container-1 div {
+  background-color: #96b1eb;
+  // 文本水平居中
+  text-align: center;
+  // 文本垂直居中
+  line-height: 100px;
+}
+
+div {
+  width: 100px;
+  height: 100px;
 }
 </style>
