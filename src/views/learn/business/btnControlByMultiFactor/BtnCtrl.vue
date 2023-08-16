3a5e4dd1b190e84d7e541b68723ad9835a5e5b39
dangfulin <2694977226@qq.cmo>
2023-08-16 13:43:39 +0800

    多因素按钮控制:完善业务需求

--- src/views/learn/business/btnControlByMultiFactor/BtnCtrl.vue
+++ src/views/learn/business/btnControlByMultiFactor/BtnCtrl.vue
@@ -2,7 +2,7 @@
   <el-button-group>
     <el-button
         v-if="isUserAuthorized"
-        @click="handleDtAuth(row)"
+        @click="handleDtAuth(props.isLocal)"
         size="small"
         type="primary"
         :icon="Finished"
@@ -12,7 +12,7 @@
 
     <el-button
         v-if="isCenterInitiated"
-        @click="handleMakeCenter(row)"
+        @click="handleMakeCenter(props.isLocal)"
         size="small"
         type="success"
         :icon="Rank"
@@ -22,14 +22,32 @@
 
     <el-button
         v-if="isBusinessAuthorized"
-        @click="handleBsnAuth(row)"
+        @click="handleBsnAuth(props.isLocal)"
         size="small"
         type="danger"
         :icon="Select"
     >
-    >
       业务授权
     </el-button>
+
+    <el-button
+        v-if="isCommitRegister"
+        @click="handleCommitRegister(props.isLocal)"
+        size="small"
+        type="danger"
+        :icon="Select"
+    >提交注册
+    </el-button>
+
+    <el-button
+        v-if="isTablePerms"
+        @click="handleTablePerms(props.isLocal)"
+        size="small"
+        type="danger"
+        :icon="Select"
+    >表共享
+    </el-button>
+
   </el-button-group>
 </template>
 
@@ -41,21 +59,28 @@ const props = defineProps({
   sysMode: Number,
   sys_admin: Boolean,
   db_admin1: Boolean,
-  row: Object,
+  isLocal: Number,
+  status: Number,
+  share: Boolean
 });
 
 const isUserAuthorized = ref(false);
 const isCenterInitiated = ref(false);
 const isBusinessAuthorized = ref(false);
+const isCommitRegister = ref(false);
+const isTablePerms = ref(false);
 
 // 在适当的时机更新计算属性的值
 watchEffect(() => {
-  isUserAuthorized.value = (props.sysMode === 0 && props.sys_admin) || ((props.sysMode === 2 || props.sysMode === 3) && (props.sys_admin || props.db_admin1));
-  isCenterInitiated.value = props.sysMode === 2 && props.sys_admin && (props.row.isLocal === 0 || props.row.isLocal === 1);
-  isBusinessAuthorized.value = props.sysMode === 2 && props.db_admin1 && props.row.isLocal === 3;
+  console.log("用户授权 watchEffect:", props)
+  isUserAuthorized.value = props.sys_admin && props.isLocal !== 2
+  isCenterInitiated.value = props.sysMode === 2 && props.sys_admin && (props.isLocal === 0 || props.isLocal === 1) && props.status === 1 && !props.share
+  isBusinessAuthorized.value = props.sysMode === 2 && props.sys_admin && props.isLocal === 3 && props.status === 1
+  isCommitRegister.value = props.sysMode === 3 && props.sys_admin && (props.isLocal === 0 || props.isLocal === 1) && props.status !== 1
+  isTablePerms.value = props.sysMode === 3 && props.sys_admin && (props.isLocal === 0 || props.isLocal === 1) && !props.share && props.status === 1
 });
 
-const emit = defineEmits(['DtAuth', 'MakeCenter', 'BsnAuth'])
+const emit = defineEmits(['DtAuth', 'MakeCenter', 'BsnAuth', 'CommitRegister', 'TablePerms'])
 
 const handleDtAuth = (row) => {
   // 抛出DtAuth事件
@@ -72,4 +97,13 @@ const handleBsnAuth = (row) => {
   emit('BsnAuth', row)
 }
 
+const handleCommitRegister = (row) => {
+  // 抛出CommitRegister事件
+  emit('CommitRegister', row)
+}
+
+const handleTablePerms = (row) => {
+  // 抛出TablePerms事件
+  emit('TablePerms', row)
+}
 </script>
--- src/views/learn/business/btnControlByMultiFactor/index.vue
+++ src/views/learn/business/btnControlByMultiFactor/index.vue
@@ -1,21 +1,168 @@
 <template>
+  <!--  <div class="container" style="margin-bottom: 15px;">-->
+  <!--    <span>选择业务用户：</span>-->
+  <!--    <el-button-->
+  <!--        type='primary' :icon="Finished"-->
+  <!--        @click='handleDtAuth()'-->
+  <!--    >按钮名称-->
+  <!--    </el-button>-->
+  <!--  </div>-->
+
   <p>基于 系统模式+当前角色+数据库类型+数据库分布类型 等因素控制按钮可用性</p>
 
-  <BtnCtrl
-      :sysMode="2"
-      :sys_admin="false"
-      :db_admin1="true"
-      :row="{isLocal: 3}"
-      @bsn-auth="handleBsnAuth"
-      @dt-auth="handleDtAuth"
-      @make-center="handleMakeCenter"
+  <span>选择系统模式：</span>
+  <el-select
+      v-model="sysMode"
+      @change="handleSysModeChange"
+      class="m-2" placeholder="Select"
+  >
+    <el-option
+        v-for="item in modeOptions"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+    />
+  </el-select>
+  <br>
+  <span>是否角色A：</span>
+  <el-switch
+      v-model="sys_admin"
+      @change="handleSysAdminChange"
   />
+  <br>
+  <span>是否角色B：</span>
+  <el-switch
+      v-model="db_admin1"
+      @change="handleDbAdmin1Change"
+  />
+  <br>
+  <span>选择数据库注册状态：</span>
+  <el-select
+      v-model="status"
+      @change="handleStatusChange"
+      class="m-2" placeholder="Select"
+  >
+    <el-option
+        v-for="item in statusOptions"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+    />
+  </el-select>
+  <br>
+  <span>选择数据库分布类型：</span>
+  <el-select
+      v-model="isLocal"
+      @change="handleIsLocalChange"
+      class="m-2" placeholder="Select"
+  >
+    <el-option
+        v-for="item in distTypeOptions"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+    />
+  </el-select>
+  <br>
+  <span>是否共享库：</span>
+  <el-switch
+      v-model="share"
+      @change="handleShareChange"
+  />
+  <br>
+
+  <el-card>
+    <BtnCtrl
+        :sysMode="sysMode"
+        :sys_admin="sys_admin"
+        :db_admin1="db_admin1"
+        :isLocal="isLocal"
+        :status="status"
+        :share="share"
+        @bsn-auth="handleBsnAuth"
+        @dt-auth="handleDtAuth"
+        @make-center="handleMakeCenter"
+        @commit-register="handleCommitRegister"
+        @table-perms="handleTablePerms"
+    />
+  </el-card>
+
 </template>
 
 <script setup>
-import {ref, defineProps, defineEmits, watchEffect} from 'vue';
+import {ref, defineProps, defineEmits, watchEffect, reactive, toRefs} from 'vue';
 import BtnCtrl from './BtnCtrl.vue';
 
+const state = reactive({
+  sysMode: 0,
+  sys_admin: false,
+  db_admin1: false,
+  isLocal: 0,
+  status: 0,
+  share: false,
+})
+
+const {
+  sysMode,
+  sys_admin,
+  db_admin1,
+  isLocal,
+  status,
+  share,
+} = toRefs(state)
+
+const modeOptions = [
+  {
+    value: 0,
+    label: '单库模式',
+  },
+  {
+    value: 2,
+    label: '中心库模式',
+  },
+  {
+    value: 3,
+    label: '子库模式',
+  }
+]
+
+const statusOptions = [
+  {
+    value: 0,
+    label: '未注册',
+  },
+  {
+    value: 1,
+    label: '已注册',
+  },
+  {
+    value: 3,
+    label: '待审核',
+  },
+  {
+    value: 4,
+    label: '已拒绝',
+  }
+]
+
+const distTypeOptions = [
+  {
+    value: 0,
+    label: '本地库',
+  },
+  {
+    value: 1,
+    label: '内域库',
+  },
+  {
+    value: 2,
+    label: '跨域库',
+  },
+  {
+    value: 3,
+    label: '中心库',
+  }
+]
 
 const handleDtAuth = (row) => {
   console.log("用户授权 handleDtAuth:", row)
@@ -28,8 +175,43 @@ const handleMakeCenter = (row) => {
 const handleBsnAuth = (row) => {
   console.log("业务授权 handleBsnAuth:", row)
 }
+
+const handleSysModeChange = (value) => {
+  // console.log("选择系统模式 handleSysModeChange:", value)
+}
+
+const handleSysAdminChange = (value) => {
+  // console.log("是否角色A handleSysAdminChange:", value)
+}
+
+const handleDbAdmin1Change = (value) => {
+  // console.log("是否角色B handleDbAdmin1Change:", value)
+}
+
+const handleIsLocalChange = (value) => {
+  console.log("选择数据库分布类型 handleIsLocalChange:", value)
+}
+
+const handleCommitRegister = (row) => {
+  console.log("提交注册 handleCommitRegister:", row)
+}
+
+const handleTablePerms = (row) => {
+  console.log("表权限 handleTablePerms:", row)
+}
+
+const handleStatusChange = (value) => {
+  console.log("选择数据库注册状态 handleStatusChange:", value)
+}
+
+const handleShareChange = (value) => {
+  // console.log("是否共享库 handleShareChange:", value)
+}
 </script>
 
 <style scoped>
-
+.container {
+  display: flex;
+  align-items: center;
+}
 </style>
