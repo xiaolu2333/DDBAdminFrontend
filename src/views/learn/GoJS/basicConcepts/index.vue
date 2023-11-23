83d30f50180010a58beafb4a12f93c995519332c
dangfulin <2694977226@qq.cmo>
2023-11-23 10:04:30 +0800

    perf：gojs 2.3.8 去除水印

--- src/views/learn/GoJS/basicConcepts/index.vue
+++ src/views/learn/GoJS/basicConcepts/index.vue
@@ -57,6 +57,42 @@ function init() {
 onMounted(() => {
   init()
 });
+
+/* gojs 2.3.8 去除水印的方法
+1，打开 node_modules/gojs/release/go-module.js 文件。
+2，node_modules/gojs/release 下有多个 js 文件，可分别在这些文件顶部添加 console.log 语句。
+3，删除 node_modules/.vite 目录，删除旧文件。
+4，npm run dev 重新启动项目，去产生新文件。
+5，node_modules/.vite/goja.js 查看具体使用的是 node_modules/gojs/release 下的哪个 js 文件，
+6，确定后进入 node_modules/gojs/release 下对应文件，搜索 document.createElement("canvas") ，找到下面这个地方：
+    function ni() {
+        if (Ng) {
+            var a = x.document.createElement("canvas"), b = a.getContext("2d"), c = Ma("7ca11abfd022028846");
+            b[c] = Ma("398c3597c01238");
+            for (var d = ["5da73c80a36455d7038e4972187c3cae51fd22", ra.Dx + "4ae6247590da4bb21c324ba3a84e385776", ld.xF + "fb236cdfda5de14c134ba1a95a2d4c7cc6f93c1387", K.za], e = 1; 5 > e; e++) b[Ma("7ca11abfd7330390")](Ma(d[e - 1]), 10, 15 * e);
+            b[c] = Ma("39f046ebb36e4b");
+            for (c = 1; 5 > c; c++) b[Ma("7ca11abfd7330390")](Ma(d[c - 1]), 10, 15 * c);
+            if (4 !== d.length || "5" !== d[0][0] || "7" !== d[3][0]) ld.prototype.Cd = ld.prototype.Cu;
+            aj = a
+        }
+    }
+7，修改最后一行：
+    function ni() {
+        if (Ng) {
+            var a = x.document.createElement("canvas"), b = a.getContext("2d"), c = Ma("7ca11abfd022028846");
+            b[c] = Ma("398c3597c01238");
+            for (var d = ["5da73c80a36455d7038e4972187c3cae51fd22", ra.Dx + "4ae6247590da4bb21c324ba3a84e385776", ld.xF + "fb236cdfda5de14c134ba1a95a2d4c7cc6f93c1387", K.za], e = 1; 5 > e; e++) b[Ma("7ca11abfd7330390")](Ma(d[e - 1]), 10, 15 * e);
+            b[c] = Ma("39f046ebb36e4b");
+            for (c = 1; 5 > c; c++) b[Ma("7ca11abfd7330390")](Ma(d[c - 1]), 10, 15 * c);
+            if (4 !== d.length || "5" !== d[0][0] || "7" !== d[3][0]) ld.prototype.Cd = ld.prototype.Cu;
+            // aj = a
+            aj = x.document.createElement("canvas");
+        }
+    }
+8，vite build --mode 打包项目
+9，另存修改后的 node_modules/gojs 以备后用
+ */
+
 </script>
 
 <style scoped>
