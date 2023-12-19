75fac6a6ae2e7b282ffc24ffba83821d22263151
xiaolu2333 <2694977226@qq.com>
2023-12-19 22:20:49 +0800

    perf:学习CSS——元素位置

---
+++ src/views/learn/learnCSS/positionControl/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1 class="title-1">positionControl-1</h1>
+    <h1 class="title-2">positionControl-2</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
