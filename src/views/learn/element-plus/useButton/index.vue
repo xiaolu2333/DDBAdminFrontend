7727e866f5c7ad5f6a292936f008131f3b6329f5
dangfulin <2694977226@qq.cmo>
2023-12-22 13:08:32 +0800

    feat：element plus——点击按钮弹出对话框后强制按钮失去焦点

--- src/views/learn/element-plus/useButton/index.vue
+++ src/views/learn/element-plus/useButton/index.vue
@@ -1,9 +1,65 @@
 <template>
-  <el-button type="primary" v-btn-lose-focus>按钮</el-button>
+  <el-button type="primary">无指令</el-button>
+  <el-button type="primary" v-btn-lose-focus>有指令</el-button>
+  <el-button
+      ref='btn3'
+      class="btn3"
+      type="primary" @click="handleClick">有弹窗
+  </el-button>
+
+  <div ref="messageRef"></div>
+
+  <el-dialog
+      title="提示"
+      v-model="dialogVisible"
+      width="30%"
+      @close="handelDialogClose"
+  >
+    11111111
+    <template #footer>
+      <el-button type="primary" @click="dialogVisible = false">取消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
 import {vBtnLoseFocus} from "@/directives/ui/elementPLus.js"
+import {ref, getCurrentInstance} from "vue";
+
+const messageRef = ref()
+const btn3 = ref()
+const dialogVisible = ref(false)
+
+// 只能在setup根级别或一些生命周期钩子中使用getCurrentInstance
+const instance = getCurrentInstance()
+console.log('instance:', instance)
+
+const handleClick = () => {
+  dialogVisible.value = true
+  // let btn33 = document.querySelector('.btn3')
+  // console.log('btn33:', btn33)
+  // btn33.blur()
+}
+
+const handelDialogClose = () => {
+  dialogVisible.value = false
+
+  console.log('messageRef.value:', messageRef.value)
+  // 随机颜色
+  messageRef.value.style.backgroundColor = '#' + Math.floor(Math.random() * 0xffffff).toString(16)
+  // 随机内容
+  messageRef.value.innerHTML = Math.floor(Math.random() * 100)
+
+
+  // // 通过按钮 ref 获取元素DOM
+  // console.log('btn3.value:', btn3.value)
+
+
+  let btn33 = document.querySelector('.btn3')
+  console.log('btn33:', btn33)
+  btn33.blur()
+}
 </script>
 
 <style scoped>
