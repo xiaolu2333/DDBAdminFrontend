80ba28d70892e6d5ffba2902989ebc8d06313b1d
dangfulin <2694977226@qq.cmo>
2024-01-25 17:38:16 +0800

    feat:element plus——树形选择器

--- src/views/learn/element-plus/useSelect/index.vue
+++ src/views/learn/element-plus/useSelect/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-4">
+  <el-card>
     <p>支持多选</p>
     <el-select
         v-model="value1"
@@ -9,15 +9,15 @@
         @change="handleChange"
     >
       <el-option
-          v-for="item in options"
+          v-for="item in options1"
           :key="item.value"
           :label="item.label"
           :value="item.value"
       />
     </el-select>
     <div>{{ value1 }}</div>
-  </div>
-  <div class="m-4">
+  </el-card>
+  <el-card>
     <p>仅单选</p>
     <el-select
         v-model="value2"
@@ -26,14 +26,25 @@
         @change="handleChange"
     >
       <el-option
-          v-for="item in options"
+          v-for="item in options1"
           :key="item.value"
           :label="item.label"
           :value="item.value"
       />
     </el-select>
     <div>{{ value2 }}</div>
-  </div>
+  </el-card>
+  <el-card>
+    <p>树形选择器</p>
+    <el-tree-select
+        v-model="value3"
+        :data="options2"
+        :props="{children: 'children',label: 'id'}"
+        :render-after-expand="false"
+        style="width: 240px"
+    />
+
+  </el-card>
 </template>
 
 <script lang="ts" setup>
@@ -43,7 +54,7 @@ import {ref} from 'vue'
 const value1 = ref(['1'])
 // 默认单选时，value是字符串
 const value2 = ref('3')
-const options = [
+const options1 = [
   {
     value: '1',
     label: 'Option1',
@@ -63,6 +74,50 @@ const options = [
   {
     value: '5',
     label: 'Option5',
+  },
+]
+
+const value3 = ref()
+const options2 = [
+  {
+    id: '1',
+    name: 'Level one 1',
+    disabled: true,
+    children: [
+      {
+        id: '1-1',
+        name: 'Level two 1-1',
+      },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Level one 2',
+    disabled: true,
+    children: [
+      {
+        id: '2-1',
+        name: 'Level two 2-1',
+      },
+      {
+        id: '2-2',
+        name: 'Level two 2-2',
+      },
+    ],
+  },
+  {
+    id: '3',
+    name: 'Level one 3',
+    children: [
+      {
+        id: '3-1',
+        name: 'Level two 3-1',
+      },
+      {
+        id: '3-2',
+        name: 'Level two 3-2',
+      },
+    ],
   },
 ]
 
