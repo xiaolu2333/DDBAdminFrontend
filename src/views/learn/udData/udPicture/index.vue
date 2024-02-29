0b6c6791d02bb158d536e7f05e17645ec7bb1337
dangfulin <2694977226@qq.cmo>
2024-02-29 17:33:55 +0800

    feat：学习——前后端数据交互——图片上传与预览——待补充

--- src/views/learn/udData/udPicture/index.vue
+++ src/views/learn/udData/udPicture/index.vue
@@ -3,6 +3,19 @@
     <el-col :span="12">
       <el-card>
         <template #header><span>上传图片</span></template>
+        <el-upload
+            v-model:file-list="fileList"
+            :auto-upload="false"
+            multiple
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+        >
+          <el-icon>
+            <Plus/>
+          </el-icon>
+        </el-upload>
+        <el-button type="primary" @click="onSubmit">点击上传</el-button>
       </el-card>
     </el-col>
     <el-col :span="12">
@@ -13,10 +26,27 @@
   </el-row>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import {ref} from 'vue'
+import {Plus} from '@element-plus/icons-vue'
 
+import type {UploadProps, UploadUserFile} from 'element-plus'
+
+const fileList = ref<UploadUserFile[]>([])
+
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+
+const handleRemove: UploadProps['onRemove'] = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+}
+
+const handlePictureCardPreview: UploadProps['onPreview'] = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url!
+  dialogVisible.value = true
+}
+
+const onSubmit = () => {
+  console.log(fileList.value)
+}
 </script>
-
-<style scoped>
-
-</style>
